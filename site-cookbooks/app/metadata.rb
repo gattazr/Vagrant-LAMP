c33f3859a7a9f02de4b5f8406f4cdc01bffdd7d4
Rémi GATTAZ <remi.gattaz@free.fr>
2014-01-01 17:10:23 -0500

    Add Apache2 The server only return 404 error

--- site-cookbooks/app/metadata.rb
+++ site-cookbooks/app/metadata.rb
@@ -3,3 +3,5 @@ maintainer        "Rémi GATTAZ"
 maintainer_email  "remi.gattaz@gmail.com"
 description       "Entry point for installing and configuring a LAMP stack"
 version           "0.1.0"
+
+depends 'apache2', '~> 1.8.0'
---
+++ site-cookbooks/app/recipes/web_server.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: app
+# Recipe:: web_server
+#
+# Copyright 2013, Rémi GATTAZ
+#
+
+# Install Apache
+include_recipe "apache2"
